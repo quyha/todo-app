ef4cc21566e9f83c5209829f6e4b37b54433b153
haquy <nguyenha.quy19@gmail.com>
2021-07-18 17:46:00 +0700

    fix: edit image path

--- src/container/task/empty-list.tsx
+++ src/container/task/empty-list.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 const TaskEmptyList: FC<{}> = () => (
     <div className="task-empty">
-        <img width={ 50 } src="/empty.svg" alt="Empty list" />
+        <img width={ 50 } src="./empty.svg" alt="Empty list" />
         <h3>Empty List</h3>
     </div>
 );
